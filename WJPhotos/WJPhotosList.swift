165d3064ee0f3aab56ca8c81af7c9de7193172b8
ulinix <ulinix@ulinixdeMacBook-Pro.local>
2018-04-10 19:42:28 +0800

    push

--- WJPhotos/WJPhotosList.swift
+++ WJPhotos/WJPhotosList.swift
@@ -12,129 +12,20 @@ import PhotosUI
 
 class WJPhotosList: UITableViewController {
  
+    lazy var photoTool = WJPhotosTool()
+    lazy var items: [WJImageAlbumItem] = []
     var allPhotos: PHFetchResult<PHAsset>!
     var smartAlbums: PHFetchResult<PHAssetCollection>!
     var userCollections: PHFetchResult<PHCollection>!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-//         Uncomment the following line to preserve selection between presentations
-         self.clearsSelectionOnViewWillAppear = false
-
-//         Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-         self.navigationItem.rightBarButtonItem = self.editButtonItem
-        
-        
-        
-//        fetchAllSystemAblum()
-//        fetchAllUserCreatedAlbum()
-      _ =   getAllSourceCollection()
-        
-    }
     
-    func fetchAllSystemAblum(){
-        let smartAlbums:PHFetchResult = PHAssetCollection.fetchAssetCollections(with: .smartAlbum, subtype: .albumRegular, options: nil)
-        print("智能\(smartAlbums.count)个")
-        
-        //smartAlbums中保存的是各个智能相册对应的PHAssetCollection
-        for i in 0..<smartAlbums.count {
-            
-            //获取一个相册(PHAssetCollection)
-            let collection = smartAlbums[i]
-            
-            if collection.isKind(of: PHAssetCollection.classForCoder()) {
-                
-                //赋值
-                let assetCollection = collection
-                
-                //从每一个智能相册获取到的PHFetchResult中包含的才是真正的资源(PHAsset)
-                let assetsFetchResults:PHFetchResult = PHAsset.fetchAssets(in: assetCollection, options: nil)
-                
-                print("\(String(describing: assetCollection.localizedTitle))相册,共有照片数:\(assetsFetchResults.count)")
-                
-                assetsFetchResults.enumerateObjects({ (asset, i, nil) in
-                    //获取每一个资源(PHAsset)
-                    print("\(asset)")
-                })
-            }
-        }
+        items = photoTool.fetchAllSystemAblum()
+//        print(items.count)
+        allPhotos = photoTool.getAllSourceCollection()
+        print("allPhotos \(allPhotos.count)")
     }
-  
-    //用户创建的相册
-    func fetchAllUserCreatedAlbum() {
-        let topLevelUserCollections:PHFetchResult = PHCollectionList.fetchTopLevelUserCollections(with: nil)
-        
-        //topLevelUserCollections中保存的是各个用户创建的相册对应的PHAssetCollection
-        print("用户创建\(topLevelUserCollections.count)个")
-        
-        for i in 0...topLevelUserCollections.count {
-            
-            //获取一个相册(PHAssetCollection)
-            let collection = topLevelUserCollections[i]
-            if collection.isKind(of: PHAssetCollection.classForCoder()) {
-                
-                //类型强制转换
-                let assetCollection = collection as! PHAssetCollection
-                
-                
-                //从每一个智能相册中获取到的PHFetchResult中包含的才是真正的资源(PHAsset)
-                let assetsFetchResults:PHFetchResult = PHAsset.fetchAssets(in: assetCollection, options: nil)
-                
-                print("\(String(describing: assetCollection.localizedTitle))相册，共有照片数:\(assetsFetchResults.count)")
-                
-                assetsFetchResults.enumerateObjects({ (asset, i, nil) in
-                    //获取每一个资源(PHAsset)
-                    print("\(asset)")
-                })
-            }
-        }
-    }
-    //由于系统返回的相册集名称为英文，我们需要转换为中文
-    private func titleOfAlbumForChinse(title:String?) -> String? {
-        if title == "Slo-mo" {
-            return "慢动作"
-        } else if title == "Recently Added" {
-            return "最近添加"
-        } else if title == "Favorites" {
-            return "个人收藏"
-        } else if title == "Recently Deleted" {
-            return "最近删除"
-        } else if title == "Videos" {
-            return "视频"
-        } else if title == "All Photos" {
-            return "所有照片"
-        } else if title == "Selfies" {
-            return "自拍"
-        } else if title == "Screenshots" {
-            return "屏幕快照"
-        } else if title == "Camera Roll" {
-            return "相机胶卷"
-        }
-        return title
-    }
-    
-    func getAllSourceCollection() -> Array<PHAsset> {
-        
-       
-        
-        var assetArray = [PHAsset]()
-        let allPhotosOptions = PHFetchOptions()
-        allPhotosOptions.sortDescriptors = [NSSortDescriptor(key: "creationDate", ascending: true)]
-        allPhotos = PHAsset.fetchAssets(with: allPhotosOptions)
-        
-        for i in 0..<allPhotos.count {
-            //获取一个资源(PHAsset)
-            let asset = allPhotos[i]
-            if allPhotos.count > 0 {
-            
-            }
-            //添加到数组
-            assetArray.append(asset)
-        }
-        return assetArray
-    }
-    
     
     
     
--- WJPhotos/WJPhotosTool.swift
+++ WJPhotos/WJPhotosTool.swift
@@ -10,112 +10,101 @@ import UIKit
 import PhotosUI
 import Photos
 
+//相簿列表项
+struct WJImageAlbumItem {
+    //相簿名称
+    var title:String?
+    //相簿内的资源
+    var fetchResult:PHFetchResult<PHAsset>
+}
+
+public enum MediaType : Int {
+    case unknown
+    case image
+    case video
+    case audio
+}
+
 class WJPhotosTool: NSObject {
     
-    var allPhotos: PHFetchResult<PHAsset>!
+    static let instance = WJPhotosTool()
+    
+//    var allPhotos: PHFetchResult<PHAsset>!
     var smartAlbums: PHFetchResult<PHAssetCollection>!
     var userCollections: PHFetchResult<PHCollection>!
     
-    func fetchAllSystemAblum(){
-        let smartAlbums:PHFetchResult = PHAssetCollection.fetchAssetCollections(with: .smartAlbum, subtype: .albumRegular, options: nil)
-        print("智能\(smartAlbums.count)个")
-        
-        //smartAlbums中保存的是各个智能相册对应的PHAssetCollection
-        for i in 0..<smartAlbums.count {
-            
-            //获取一个相册(PHAssetCollection)
-            let collection = smartAlbums[i]
-            
-            if collection.isKind(of: PHAssetCollection.classForCoder()) {
-                
-                //赋值
-                let assetCollection = collection
-                
-                //从每一个智能相册获取到的PHFetchResult中包含的才是真正的资源(PHAsset)
-                let assetsFetchResults:PHFetchResult = PHAsset.fetchAssets(in: assetCollection, options: nil)
-                
-                print("\(String(describing: assetCollection.localizedTitle))相册,共有照片数:\(assetsFetchResults.count)")
-                
-                assetsFetchResults.enumerateObjects({ (asset, i, nil) in
-                    //获取每一个资源(PHAsset)
-                    print("\(asset)")
-                })
-            }
-        }
+    //相簿列表项集合
+    fileprivate var items:[WJImageAlbumItem] = []
+    
+    //MARK: -- 获取所有照片
+    public func getAllSourceCollection() -> PHFetchResult<PHAsset> {
+        let fetchOptions: PHFetchOptions = requestPHFetchOptions(nil)
+        let allPhotos = PHAsset.fetchAssets(with: fetchOptions)
+        return allPhotos
     }
     
-    //用户创建的相册
-    func fetchAllUserCreatedAlbum() {
+    //MARK: -- 智能相册列表
+    public func fetchAllSystemAblum() -> [WJImageAlbumItem] {
+        let fetchOptions: PHFetchOptions = requestPHFetchOptions(MediaType.image)
+        
+        let smartAlbums:PHFetchResult = PHAssetCollection.fetchAssetCollections(with: .smartAlbum, subtype: .albumRegular, options: nil)
+        //smartAlbums中保存的是各个智能相册对应的PHAssetCollection
+        for i in 0..<smartAlbums.count {
+            //获取一个相册(PHAssetCollection)
+            let collection = smartAlbums[i]
+            if collection.isKind(of: PHAssetCollection.classForCoder()) {
+                //赋值
+                let assetCollection = collection
+                //从每一个智能相册获取到的PHFetchResult中包含的才是真正的资源(PHAsset)
+                let assetsFetchResults:PHFetchResult = PHAsset.fetchAssets(in: assetCollection, options: fetchOptions)
+                assetsFetchResults.enumerateObjects({ (asset, i, nil) in })
+                if assetsFetchResults.count > 0 {
+                    items.append(WJImageAlbumItem(title: assetCollection.localizedTitle, fetchResult: assetsFetchResults))
+                }
+            }
+        }
+        return items
+    }
+    
+    //MARK: -- 用户创建的相册
+    public func fetchAllUserCreatedAlbum() -> [WJImageAlbumItem] {
+        let fetchOptions: PHFetchOptions = requestPHFetchOptions(MediaType.image)
+        
         let topLevelUserCollections:PHFetchResult = PHCollectionList.fetchTopLevelUserCollections(with: nil)
-        
-        //topLevelUserCollections中保存的是各个用户创建的相册对应的PHAssetCollection
-        print("用户创建\(topLevelUserCollections.count)个")
-        
         for i in 0...topLevelUserCollections.count {
-            
             //获取一个相册(PHAssetCollection)
             let collection = topLevelUserCollections[i]
             if collection.isKind(of: PHAssetCollection.classForCoder()) {
-                
                 //类型强制转换
                 let assetCollection = collection as! PHAssetCollection
-                
-                
                 //从每一个智能相册中获取到的PHFetchResult中包含的才是真正的资源(PHAsset)
-                let assetsFetchResults:PHFetchResult = PHAsset.fetchAssets(in: assetCollection, options: nil)
-                
-                print("\(String(describing: assetCollection.localizedTitle))相册，共有照片数:\(assetsFetchResults.count)")
-                
-                assetsFetchResults.enumerateObjects({ (asset, i, nil) in
-                    //获取每一个资源(PHAsset)
-                    print("\(asset)")
-                })
+                let assetsFetchResults:PHFetchResult = PHAsset.fetchAssets(in: assetCollection, options: fetchOptions)
+                assetsFetchResults.enumerateObjects({ (asset, i, nil) in })
+                if assetsFetchResults.count > 0 {
+                    items.append(WJImageAlbumItem(title: assetCollection.localizedTitle, fetchResult: assetsFetchResults))
+                }
             }
         }
-    }
-    //由于系统返回的相册集名称为英文，我们需要转换为中文
-    private func titleOfAlbumForChinse(title:String?) -> String? {
-        if title == "Slo-mo" {
-            return "慢动作"
-        } else if title == "Recently Added" {
-            return "最近添加"
-        } else if title == "Favorites" {
-            return "个人收藏"
-        } else if title == "Recently Deleted" {
-            return "最近删除"
-        } else if title == "Videos" {
-            return "视频"
-        } else if title == "All Photos" {
-            return "所有照片"
-        } else if title == "Selfies" {
-            return "自拍"
-        } else if title == "Screenshots" {
-            return "屏幕快照"
-        } else if title == "Camera Roll" {
-            return "相机胶卷"
-        }
-        return title
+        return items
     }
     
-    func getAllSourceCollection() -> Array<PHAsset> {
-        
-        
-        
-        var assetArray = [PHAsset]()
-        let allPhotosOptions = PHFetchOptions()
-        allPhotosOptions.sortDescriptors = [NSSortDescriptor(key: "creationDate", ascending: true)]
-        allPhotos = PHAsset.fetchAssets(with: allPhotosOptions)
-        
-        for i in 0..<allPhotos.count {
-            //获取一个资源(PHAsset)
-            let asset = allPhotos[i]
-            if allPhotos.count > 0 {
-                
-            }
-            //添加到数组
-            assetArray.append(asset)
+   
+    //MARK: -- 筛选条件
+    public func requestPHFetchOptions(_ mediaType: MediaType?) -> PHFetchOptions {
+        let fetchOptions = PHFetchOptions()
+        fetchOptions.sortDescriptors = [NSSortDescriptor(key: "creationDate", ascending: true)]
+        switch mediaType {
+        case MediaType.unknown?:
+            fetchOptions.predicate = NSPredicate(format: "mediaType = %d", PHAssetMediaType.unknown.rawValue)
+        case MediaType.video?:
+            fetchOptions.predicate = NSPredicate(format: "mediaType = %d", PHAssetMediaType.video.rawValue)
+        case MediaType.audio?:
+            fetchOptions.predicate = NSPredicate(format: "mediaType = %d", PHAssetMediaType.audio.rawValue)
+        default:
+            fetchOptions.predicate = NSPredicate(format: "mediaType = %d", PHAssetMediaType.image.rawValue)
         }
-        return assetArray
+        return fetchOptions
     }
+    
 
 }
